75f1750fc3e7bc22971a0633d1f48390d97fecd4
krosenk729 <krosenk729@gmail.com>
2018-05-19 07:21:35 -0400

    changing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals')
 const StartServerPlugin = require('start-server-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  // mode: 'development',
   entry: ['webpack/hot/poll?1000', './src/index'],
   watch: true,
   devtool: 'sourcemap',
